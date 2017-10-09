2bb0643b1f3424177fa519dd54fccac9ee9d1fac
rosie w <rosie.wilt@gmail.com>
2017-10-09 21:57:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,13 +14,6 @@ function add2(n) {
   return n + two  
 }
 
-describe('add2(n)', function() {
-  it('adds two to n', function() {
-    const n = Math.floor(Math.random() * 1000)
-    expect(window.add2(n)).toEqual(n + 2)
-  })
-})
-
 var funkyFunction = function() {
   return function() {
     return "FUNKY!"
